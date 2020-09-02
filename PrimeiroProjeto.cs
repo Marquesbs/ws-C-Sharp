45ee2972ff8e632684175323f7dbf90cd2b82600
Bruno Marques <marques.bs@outlook.com.br>
2020-09-02 02:52:10 -0300

    Update and rename Primeiro.cs to PrimeiroProjeto.cs

---
+++ PrimeiroProjeto.cs
@@ -0,0 +1,12 @@
+﻿using System; //referencia para o namespace chamado System, namespace padrão que contém funções básicas de manipulação de programas.
+
+namespace PrimeiroProjeto //declaração de namespace desse arquivo. (não obrigatório, questão de organização)
+{
+    class Program //declaração da classe, unidade básica da POO.
+    {
+        static void Main(string[] args) //membro da classe - declaração padrão da linguagem para identificar o Entry Point da aplicação
+        { //tudo colocado aqui será executado primeiro no programa
+            Console.WriteLine("Olá Mundo!"); 
+        }
+    }
+}
