1b0469937862912b628dc66bdce5508bb311d419
Bruno Marques <marques.bs@outlook.com.br>
2020-09-05 20:48:22 -0300

    Add files via upload

---
+++ variaveisMinMax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace projeto4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.MaxValue;
+            int n2 = int.MinValue;
+
+            sbyte n3 = sbyte.MaxValue;
+            sbyte n4 = sbyte.MinValue;
+
+            decimal n5 = decimal.MaxValue;
+
+            System.Console.WriteLine(n1);
+            System.Console.WriteLine(n2);
+            System.Console.WriteLine(n3);
+            System.Console.WriteLine(n4);
+            System.Console.WriteLine(n5);
+        }
+    }
+}
