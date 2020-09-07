d10a68c609c4c45bfa98d6e1f4838d8e2d3e70e0
Bruno Marques <marques.bs@outlook.com.br>
2020-09-07 00:39:40 -0300

    Add files via upload

---
+++ exercicioDeFixacao02.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System;
+
+namespace exercicioDeFixacao
+{
+    class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Entre com o seu nome completo: ");
+            string nome = Console.ReadLine();
+            System.Console.Write("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+            System.Console.Write("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+            string last = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine(nome);
+            System.Console.WriteLine(quartos);
+            System.Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine();
+            System.Console.WriteLine(last);
+            System.Console.WriteLine(idade);
+            System.Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
