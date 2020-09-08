7d0faa08605ed2a39a0b6a0741958225fe6feebf
Bruno Marques <marques.bs@outlook.com.br>
2020-09-07 22:11:04 -0300

    Add files via upload

---
+++ exercicios_estrutura_sequencial2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicios_estrutura_sequencial
+{
+    class exercicio02
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, pi;
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = pi * (Math.Pow(raio,2));
+
+            System.Console.WriteLine("A = " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
