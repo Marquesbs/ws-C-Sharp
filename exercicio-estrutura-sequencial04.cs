92a5f4f498d101218cd820b844be4d13c2a25e7a
Bruno Marques <marques.bs@outlook.com.br>
2020-09-08 20:54:54 -0300

    Create exercicio-estrutura-sequencial04.cs

---
+++ exercicio-estrutura-sequencial04.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+
+namespace exercicios_estrutura_sequencial
+{
+    class Exercicio04
+    {
+        static void Main(string[] args)
+        {
+            int funcionario, hora_trabalhada;
+            double recebe_hora, salario;
+            
+            funcionario = int.Parse(Console.ReadLine());
+            hora_trabalhada = int.Parse(Console.ReadLine());
+            recebe_hora = double.Parse(Console.Readline(), CultureInfo.InvariantCulture);
+            
+            salario = hora_trabalhada * recebe_hora;
+            
+            System.Console.WriteLine("NUMBER = " + funcionario);
+            System.Console.WriteLine("SALARY = U$ " + salario.ToString("F2".CultureInfo.InvariantCulture));          
+        }
+    }
+}
