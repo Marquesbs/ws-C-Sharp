c562c32b68cae0bf3da874b6394208d7697d2856
Bruno Marques <marques.bs@outlook.com.br>
2020-09-15 05:16:44 -0300

    Add files via upload

---
+++ exercicios-estrutura-repetitiva-while01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+            while (senha != 2002)
+            {
+                System.Console.WriteLine("Senha Inválida!");
+                senha = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
