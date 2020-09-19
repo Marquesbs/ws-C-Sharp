60f2f028526c6b1566de3ee1e5b5e2fd5365ea77
Bruno Marques <marques.bs@outlook.com.br>
2020-09-18 21:19:17 -0300

    Add files via upload

---
+++ exercicios-estrutura-repetitiva-for02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qntidade = int.Parse(Console.ReadLine());
+            int n = 0, dentro = 0, fora = 0;
+
+            for (int contador = 1;contador <= qntidade; contador++)
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n >= 10 && n <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            }
+            System.Console.WriteLine("{0} in", dentro);
+            System.Console.WriteLine("{0} out", fora);
+        }
+    }
+}
