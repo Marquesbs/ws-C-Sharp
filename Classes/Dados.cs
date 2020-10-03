6f2284f26838e75440ffb51fb3a37d60b7332154
Bruno Marques <marques.bs@outlook.com.br>
2020-10-03 01:21:57 -0300

    Add files via upload

---
+++ Classes/Dados.cs
@@ -0,0 +1,8 @@
+namespace Projeto15
+{
+    public class Dados
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Classes/Exercicio_Classe01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Projeto15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados pessoa1, pessoa2;
+            System.Console.WriteLine("Dados da primeira pessoa: ");
+
+            pessoa1 = new Dados();
+            pessoa2 = new Dados();
+
+            pessoa1.nome = Console.ReadLine();
+            pessoa1.idade = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Dados da segunda pessoa: ");;
+            pessoa2.nome = Console.ReadLine();
+            pessoa2.idade = int.Parse(Console.ReadLine());
+
+            if (pessoa1.idade > pessoa2.idade)
+            {
+                System.Console.WriteLine("Pessoa mais velha: " + pessoa1.nome);
+            }
+            else
+            {
+                System.Console.WriteLine("Pessoa mais velha: " + pessoa2.nome);
+            }
+        }
+    }
+}
