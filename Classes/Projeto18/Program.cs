3bfb7425fb05130c1ef17290eb98cdc7bd8722a0
Bruno Marques <marques@pop-os.localdomain>
2020-10-03 02:01:52 -0300

    Added Program.cs

---
+++ Classes/Projeto18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto prod, valor, qnt;
+            prod = new Produto();
+            valor = new Produto();
+            qnt = new Produto();
+
+            System.Console.Write("Insira o nome do Produto: ");
+            prod.nome = Console.ReadLine();
+            System.Console.Write("Insira o preço do Produto: ");
+            valor.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine("Insira a quantidade: ");
+            qnt.quantidade = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
