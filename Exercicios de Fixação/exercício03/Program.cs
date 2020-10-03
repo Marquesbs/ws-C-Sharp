41086e4e6afe6d50d8ad789cd5717e7307d4cc7f
Bruno Marques <marques.bs@outlook.com.br>
2020-10-03 03:41:30 -0300

    criado nova pasta de exercicio

---
+++ Exercicios de Fixação/exercício03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercício03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
