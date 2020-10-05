ebfae4687d9bd1c8f1b43cf79873a22468ea232a
Bruno Marques <marques.bs@outlook.com.br>
2020-10-05 00:40:11 -0300

    Upload dos arquivos do exercicio de fixaçao 05

---
+++ Exercicios de Fixação/exercício05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercício05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno al = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            al.Nome = Console.ReadLine();
+            System.Console.WriteLine("Digite as três notas do aluno:");
+            al.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("NOTA FINAL = " + al.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (al.Aprovado()) 
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + al.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
