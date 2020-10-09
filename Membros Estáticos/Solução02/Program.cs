ffb78296e7f0ae4cc929b3c43f67bb15ff8d26cb
Bruno Marques <marques.bs@outlook.com.br>
2020-10-09 04:14:01 -0300

    upload arquivo membros estaticos soluçao02

---
+++ Membros Estáticos/Solução02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Solução02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine());
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            System.Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
