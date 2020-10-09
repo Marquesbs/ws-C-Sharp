b4bd3ae9eec93ce281da38bb5390e4dcea458b9d
Bruno Marques <marques.bs@outlook.com.br>
2020-10-08 22:43:51 -0300

    Adicionada novas pastas para novos exercicios

---
+++ Membros Estáticos/Solução01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solução01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
