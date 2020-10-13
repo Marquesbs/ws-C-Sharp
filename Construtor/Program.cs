3c94f480c130b5a7525c3e3bb2d7f0e82f8e07bf
Bruno Marques <marques.bs@outlook.com.br>
2020-10-12 21:26:15 -0300

    Exemplo Construtor

---
+++ Construtor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+            
+            Console.WriteLine("\nDados do produto: " + p);
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            
+            Console.WriteLine("\nDados atualizados: " + p);
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            
+            Console.WriteLine("\nDados atualizados: " + p);
+        }
+    }
+}
